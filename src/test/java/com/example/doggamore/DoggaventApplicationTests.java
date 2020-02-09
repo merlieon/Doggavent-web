9c33b78d6a69d53f65d7262c29ef39bd3402130a
merlieon <marcus.wilen@hotmail.com>
2020-02-09 11:37:09 +0100

    first up

---
+++ src/test/java/com/example/doggamore/DoggaventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doggamore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggaventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
